d8a9a41eab4bf8886bc7d54a29c699782f4c1402
Bobby Hensley <49879004+bhensley0440@users.noreply.github.com>
2020-05-22 10:58:44 -0600

    HOPB 2019 controls

--- hydrologicControls/D01_HOPB/HOPB_controls_20170921.R
+++ hydrologicControls/D01_HOPB/HOPB_controls_20170921.R
@@ -35,9 +35,9 @@ filepath <- "N:/Science/AQU/Controls/D01_HOPB_20170921"
 URIpath <- paste(filepath,"filesToStack00131","stackedFiles",sep = "/")
 
 # #Download data from API and store somewhere
-# dataFromAPI <- neonUtilities::zipsByProduct(streamMorphoDPID,siteID,package="expanded",check.size=FALSE,savepath = filepath)
-# neonUtilities::stackByTable(filepath=paste(filepath,"filesToStack00131",sep = "/"), folder = TRUE)
-# neonUtilities::zipsByURI(filepath=URIpath, savepath = URIpath, pick.files=FALSE, unzip = TRUE, check.size = FALSE)
+dataFromAPI <- neonUtilities::zipsByProduct(streamMorphoDPID,siteID,startdate="2017-01", enddate="2017-12",package="expanded",check.size=FALSE,savepath = filepath)
+neonUtilities::stackByTable(filepath=paste(filepath,"filesToStack00131",sep = "/"), folder = TRUE)
+neonUtilities::zipsByURI(filepath=URIpath, savepath = URIpath, pick.files=FALSE, unzip = TRUE, check.size = FALSE)
 
 #Read in downloaded data
 surveyPtsDF <- read.table(paste0(URIpath,"/NEON_D01_HOPB_GEOMORPH_20170921_L0_VE/HOPB_surveyPts_20170921.CSV"),
