3f648376a5fe1653b18c30dc5cfbcd18b973954c
znickerson8 <nickerson@battelleecology.org>
2021-06-30 16:25:39 -0600

    Update baseCodeForApp.R

--- L4Discharge/AOSApp/baseCodeForApp.R
+++ L4Discharge/AOSApp/baseCodeForApp.R
@@ -1,5 +1,22 @@
+library(tidyverse)
 
+site <- "HOPB"
+startDate <-  "2018-01-01"
+endDate <- "2019-01-01"
 
-site <- ""
-startDate <-  ""
-endDate <- ""
+searchIntervalStartDate <- as.character(stageQCurve::def.calc.WY.strt.end.date(searchIntervalStartDate = startDate)$startDate)
+searchIntervalEndDate <- as.character(stageQCurve::def.calc.WY.strt.end.date(searchIntervalStartDate = endDate)$endDate)
+
+# Get continuous discharge data from the NEON API
+DP4.00133.001 <- neonUtilities::loadByProduct(
+  dpID="DP4.00133.001",
+  token = Sys.getenv("NEON_PAT"),
+  package = "basic",
+  check.size = F,
+  site = site,
+  startdate = searchIntervalStartDate,
+  enddate = searchIntervalEndDate
+)
+gaugeDischargeMeas <- DP4.00133.001$sdrc_gaugeDischargeMeas%>%
+  separate(gaugeEventID,c("site","date"),5,remove = F)%>%
+  mutate(date=paste0(as.Date(date,format="%Y%m%d")," 12:00:00"))
