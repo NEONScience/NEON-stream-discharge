848c6c284107ffab6482694b1a3a59e6787943c1
lucasjsmith5040 <63403968+lucasjsmith5040@users.noreply.github.com>
2024-08-02 12:28:47 -0400

    Added the necessary functions and computations to convert L0 pressure to calculated gauge height
    - Added TLOC function from L4 Discharge API
    - Added regression data query and formatting from L4 Discharge
    - Formatted UI of the calculated gauge height tabs
    - Updated naming of various UI elements for gauge height tabs
    - Added make.flat function from L4 Discharge
    - Changed Intro page to HTML file for styling purposes. Currently has an issue with images (WIP)
    - removed geoNEON usage throughout code
    - Added dischargeQuery for getting L4 staff gauge data

--- shiny-openFlow/src/functions/blueHeronFunctions.R
+++ shiny-openFlow/src/functions/blueHeronFunctions.R
@@ -1,31 +1,39 @@
 #Blue Heron water height calculation functions
 
 #Water pressure function, calculates water pressure from calibration and troll pressure values
-waterPressure_calc <- function(calValCP0,calValCP1, calValCP2, trollPressure)
+waterPressure_calibration <- function(calValCP0,calValCP1, calValCP2, trollPressure)
 {
-  waterPressure <- calValCP2*(trollPressure^2)+calValCP1*trollPressure+calValCP0
-  return(waterPressure)
+  waterPressureCalibrated <- calValCP2*trollPressure**2+calValCP1*trollPressure+calValCP0
+  return(waterPressureCalibrated)
 }
 
 #water depth function, calculates water depth from water pressure, density, and gravity
-waterDepth_calc <- function(waterPressure)
+waterColumn_calc <- function(waterPressureCalibrated)
 {
-  waterDepth <- round(1000*(waterPressure/(999*9.81)),4) # 9.81 is gravity and 999 is the water density. This also converts kPA to PA.
-  return(waterDepth)
+  waterColumnHeight <- (waterPressureCalibrated/(waterDensity*gravity)) * convKPatoPa # 9.81 is gravity and 999 is the water density. This also converts kPA to PA.
+  
+  if(exists("wellDepth") & exists("cableLength"))
+  {
+    if(!is.null(wellDepth) & !is.null(cableLength))
+    {
+      waterColumnHeight <- round(waterColumnHeight + wellDepth - cableLength,4)
+    }
+  }
+  
+  return(waterColumnHeight)
 }
 
-waterColumnHeight_calc <- function(waterDepth, wellDepth, cableLength)
-{
-  if(!is.null(wellDepth) & !is.null(cableLength))
-  {
-    waterColumnHeight <- round(waterDepth + wellDepth - cableLength,4)
-  }
-}
+# wellWaterColumn_calc <- function(waterDepth, wellDepth, cableLength)
+# {
+#   if(!is.null(wellDepth) & !is.null(cableLength))
+#   {
+#     waterColumnHeight <- round(waterDepth + wellDepth - cableLength,4)
+#   }
+# }
 
 
 BlueHeron <- function(input, output, session, realTimeSingleInput = NULL, realTimeSeriesInput = NULL)
 {
-  shinyjs::show("LB")
   tryCatch({
     
     #set DPname to either 'SW_elev' or 'GW_elev' allows for AquaTROLL and LevelTROLL for SW locations dependent on user selection
@@ -52,7 +60,7 @@ BlueHeron <- function(input, output, session, realTimeSingleInput = NULL, realTi
     endDate <- input$BH_dateRange[2]
     
     #Sets the HOR for the location of interest (surface water locations are 101,102,131,132,110 and groundwater locations are 301-308 for wells 1-8)
-    HOR <<- as.character(input$HOR)
+    HOR <- as.character(input$HOR)
     
     #Compiles DP Ids
     if(DPname=='GW_cond'){
@@ -80,58 +88,52 @@ BlueHeron <- function(input, output, session, realTimeSingleInput = NULL, realTi
     
     # Troubleshooting information, commented out unless needed
     # domain = "D07"
-    # site = "WALK"
-    # HOR = "101"
+    # site = "LECO"
+    # HOR = "102"
     # startDate = "2023-01-01"
     # endDate = "2023-01-28"
     # DPnum<-"DP0.20016.001.01379"
     # VER<-"100"
     # End troubleshooting information
-    DPID = paste("NEON",domain,site,DPnum,HOR,VER,"000",sep=".")
+    DPID <- paste("NEON",domain,site,DPnum,HOR,VER,"000",sep=".")
     
     #Pulls calibration information
-    calibrations<-grabCalibrations(CDSURL = "http://den-prodcdsllb-1.ci.neoninternal.org/cdsWebApp/",
+    calibrations <- grabCalibrations(CDSURL = "http://den-prodcdsllb-1.ci.neoninternal.org/cdsWebApp/",
                                    startDate = startDate,
                                    endDate = endDate,
                                    DPID = DPID,
                                    session,
                                    input,
                                    output)
-    #STEP 2: Grab sensor elevation data
+    # warning("It appears there is an issue with the settings selected. Please try different settings.")
     
-    #Get site history for all named locations. I have not included any elevation information at this time. It could be added.
-    updateProgressBar(session = session, id = "spatialDataLB", value = 35, title = "Gathering spatial data")
-    locations <- getLocBySite(site, type = "all", history = T)
-    updateProgressBar(session = session, id = "spatialDataLB", value = 75, title = "Finished gathering spatial data")
-    locations_troll<-locations[grepl("Water Level",locations$locationDescription),]
-    locations_wells<-locations[grepl("Groundwater Well ",locations$locationDescription),]
-    locations<-rbind(locations_troll,locations_wells)
-    #locations<-locations[!grepl("Not Used",locations$locationDescription),]
-    
-    #No information is used from this to calculate water depth. Elevated water depth and other elevation information could be added if needed or wanted.
-    TrollLocations<<-locations %>%
-      select('namedLocation','locationDescription', 'locationStartDate', 'locationEndDate','domainID','siteID','elevation','zOffset')
-    
-    
+    #grabs the relevant calibration information from the downloaded calibrations data
+    calibration_info <- as.data.frame(rbind(calibrations$value[1:3]))
+    colnames(calibration_info) <- c("calValCP0","calValCP1","calValCP2")
+    # Downloads location data and formats/filters the data
+
     if(input$L0Choice == "Yes")
     {
       shinyjs::hide("singleWaterColumnHeight")
-      shinyjs::hide("singlePressureOutput")
       #Grab L0 data from local .csv download of L0 data
-      if(is.null(realTimeSeriesInput))
+      if(is.null(input$L0File))
       {
         #Query L0 data
-        trollL0Data <- processWaterData(input, output, session, DPID = DPID, startDate = startDate, endDate = endDate)
+        L0TrollInformation <- L0DataQuery(input, output, session, DPID = DPID, startDate = startDate, endDate = endDate)
+        trollL0Data <- xmlToDataFrame(L0TrollInformation$L0Data)
+        trollL0Data <- trollL0Data[-1,]
+        trollNamedLocation <- as.data.frame(L0TrollInformation$namedLocation)
       } else {
-        trollL0Data <- read.csv("tempBHData/Data.csv")
+        trollL0Data <- read.csv(input$L0File$datapath) %>% 
+          select(paste0("NEON.",domain,".",site,".DP0.20016.001.01379.132.100.000.time"), paste0("NEON.",domain,".",site,".DP0.20016.001.01379.132.100.000.data"))
+        colnames(trollL0Data) <- c("startDate","numberValue")
+        startDate <- min(trollL0Data$startDate)
+        endDate <- min(trollL0Data$endDate)
       }
-
-      #grabs the relevant calibration information from the downloaded calibrations data
-      calibration_info <- as.data.frame(rbind(calibrations$value[1:3]))
-      colnames(calibration_info) <- c("calValCP0","calValCP1","calValCP2")
+      
       #combine the trollPressure, Dates, and calibrations information into one DF
-      waterElevationDF <- as.data.frame(cbind(as.character(trollL0Data$startDate), as.numeric(trollL0Data$numberValue), as.numeric(rep(calibration_info$calValCP0)), as.numeric(rep(calibration_info$calValCP1)), as.numeric(rep(calibration_info$calValCP2)), rep(999), rep(9.81)))
-      colnames(waterElevationDF) <- c("Date","trollPressure", "calValCP0","calValCP1","calValCP2", "waterDensity", "Gravity")
+      waterElevationDF <- as.data.frame(cbind(as.character(trollL0Data$startDate), as.numeric(trollL0Data$numberValue), as.character(trollNamedLocation$name), as.numeric(rep(calibration_info$calValCP0)), as.numeric(rep(calibration_info$calValCP1)), as.numeric(rep(calibration_info$calValCP2)), rep(waterDensity), rep(gravity)))
+      colnames(waterElevationDF) <- c("Date","trollPressure", "namedLocation", "calValCP0","calValCP1","calValCP2", "waterDensity", "Gravity")
       
       #For some reason when combining even as.numeric the variables below come out as character columns. Re-applying the command seems to work.
       waterElevationDF$trollPressure <- as.numeric(trollL0Data$numberValue)
@@ -140,95 +142,76 @@ BlueHeron <- function(input, output, session, realTimeSingleInput = NULL, realTi
       waterElevationDF$calValCP2 <- as.numeric(waterElevationDF$calValCP2)
       
       #Calculate water pressure
-      waterPressure <- waterPressure_calc(calValCP0 = waterElevationDF$calValCP0, calValCP1 = waterElevationDF$calValCP1,calValCP2 = waterElevationDF$calValCP2, trollPressure = waterElevationDF$trollPressure)
+      waterPressureCalibrated <- waterPressure_calibration(calValCP0 = waterElevationDF$calValCP0, calValCP1 = waterElevationDF$calValCP1,calValCP2 = waterElevationDF$calValCP2, trollPressure = waterElevationDF$trollPressure)
       #Calculate water depth 
-      waterDepth <- waterDepth_calc(waterPressure)
+      waterColumnHeight <- waterColumn_calc(waterPressureCalibrated)
       #Add water pressure and depth to DF
-      waterElevationDF <- cbind(waterElevationDF,waterPressure,waterDepth)
+      waterElevationDF <- cbind(waterElevationDF,waterPressureCalibrated,waterColumnHeight)
       
-      #adjust for elevation offset
-      TrollLocations <- TrollLocations[order(TrollLocations$locationEndDate)]
-      TrollLocations$refElevPlusZ <- TrollLocations$elevation + TrollLocations$zOffset
-      for(i in 1:length(TrollLocations$locationEndDate))
+      waterElevationDF <- TLOC(input, output, session, site, startDate, endDate, waterElevationDF)
+      waterElevationDFSW <<- waterElevationDF
+      if(input$waterType == "SW")
       {
-        locStart <- TrollLocations$locationStartDate[i]
-        locEnd <- TrollLocations$locationEndDate[i]
-        dataToApplyOffset <- waterElevationDF$waterDepth[waterElevationDF$Date>=locStart&
-                                                           waterElevationDF$Date<locEnd]
-        if(i==1)
-        {
-          locOffset <- 0
-          waterElevationDF$waterDepth[waterElevationDF$Date>=locStart&
-                                        waterElevationDF$Date<locEnd] <- dataToApplyOffset+as.numeric(locOffset)
-        } else {
-          locOffset <- TrollLocations$refElevPlusZ[i] - TrollLocations$refElevPlusZ[1]
-          waterElevationDF$waterDepth[waterElevationDF$Date>=locStart&
-                                                             waterElevationDF$Date<locEnd] <- dataToApplyOffset+as.numeric(locOffset)
+        regressionData <- formatReg(input, output, session)
+        waterElevationDFBEFORE <<- waterElevationDF
+        waterElevationDF <- applyRegtoL0(regressionData = regressionData, L0PressureData = waterElevationDF, site = site, startDate = startDate, endDate = endDate, session) 
+        waterElevationDFAFTER <<- waterElevationDF
+        output$calculatedStagePlot <- renderPlotly({
+          waterElevationPlot <- plot_ly(waterElevationDF, x = ~Date, y = ~calculatedStage, type = 'scatter', mode = 'lines', fill = 'tozeroy') %>% 
+            layout(xaxis= list(title = "Date",autotick = T,nticks = 25, tickmode = "auto"), yaxis = list(title = 'Calculated stage height (m)'))
+          }) #End of waterElevation output
         }
-      }
-      
-      #Add regression ID to the L0 pressure data
-      waterElevationDF$regressionID <- NA
-      for (i in 1:nrow(regressionData)) {
-        waterElevationDF$regressionID[waterElevationDF$Date>=regressionData$regressionStartDate[i]&waterElevationDF$Date<=regressionData$regressionEndDate[i]] <- regressionData$regressionID[i]
-      }
-      waterElevationDF$regressionID[is.na(waterElevationDF$regressionID)&waterElevationDF$Date>max(regressionData$regressionEndDate)] <- regressionData$regressionID[regressionData$regressionEndDate==max(regressionData$regressionEndDate)]
-      
-      # Calculate stage
-      waterElevationDF$calculatedStage <- NA
-      for (i in 1:nrow(regressionData)) {
-        waterElevationDF$calculatedStage[waterElevationDF$regressionID==regressionData$regressionID[i]] <- (waterElevationDF$waterColumnHeight[waterElevationDF$regressionID==regressionData$regressionID[i]]*regressionData$regressionSlope[i])+regressionData$regressionIntercept[i]
-      }
-      
-      output$waterElevation <- renderPlotly({
-        waterElevationPlot <- plot_ly(waterElevationDF, x = ~Date, y = ~waterDepth, type = 'scatter', mode = 'lines') %>% layout(xaxis= list(title = "Date",autotick = T,nticks = 25, tickmode = "auto"), yaxis = list(title = 'water depth above sensor'))
-      }) #End of waterElevation output
-      shinyjs::show("waterElevation")
+        else {
+          output$calculatedStagePlot <- renderPlotly({
+            waterElevationPlot <- plot_ly(waterElevationDF, x = ~format(Date, "%Y-%m-%d %H:%M"), y = ~waterColumnHeight, type = 'scatter', mode = 'lines', fill = 'tozeroy') %>% 
+              layout(xaxis= list(title = "Date",autotick = T,nticks = 25, tickmode = "auto"), yaxis = list(title = 'Water column height (m)'))
+            }) #End of waterElevation output
+          }
+
+
+      shinyjs::show("calculatedStagePlot")
       shinyjs::show("Title_CWE")
       
-      #Add water column height graph if information is provided
-      if(!is.na(input$wellDepth) & !is.na(input$cableLength) & input$waterType == "GW")
+    } else{ # Single input pressure from user on else
+      shinyjs::hide("calculatedStagePlot")
+      trollNamedLocation <- L0DataQuery(input, output, session, DPID = DPID, startDate = startDate, endDate = endDate)
+      
+      #combine the trollPressure, Dates, and calibrations information into one DF
+      waterElevationDF <- as.data.frame(cbind(as.character(startDate), as.numeric(input$singlePressure), as.character(trollNamedLocation), as.numeric(rep(calibration_info$calValCP0)), as.numeric(rep(calibration_info$calValCP1)), as.numeric(rep(calibration_info$calValCP2)), rep(waterDensity), rep(gravity)))
+      colnames(waterElevationDF) <- c("Date","trollPressure", "namedLocation","calValCP0","calValCP1","calValCP2", "waterDensity", "Gravity")
+      
+      #For some reason when combining even as.numeric the variables below come out as character columns. Re-applying the command seems to work.
+      waterElevationDF$trollPressure <- as.numeric(input$singlePressure)
+      waterElevationDF$calValCP0 <- as.numeric(waterElevationDF$calValCP0)
+      waterElevationDF$calValCP1 <- as.numeric(waterElevationDF$calValCP1)
+      waterElevationDF$calValCP2 <- as.numeric(waterElevationDF$calValCP2)
+      
+      #Calculate water pressure
+      waterPressureCalibrated <- waterPressure_calibration(calValCP0 = waterElevationDF$calValCP0, calValCP1 = waterElevationDF$calValCP1,calValCP2 = waterElevationDF$calValCP2, trollPressure = waterElevationDF$trollPressure)
+      #Calculate water depth 
+      waterColumnHeight <- waterColumn_calc(waterPressureCalibrated)
+      #Add water pressure and depth to DF
+      waterElevationDF <- cbind(waterElevationDF,waterPressureCalibrated,waterColumnHeight)
+      waterElevationDF <- TLOC(input, output, session, site, startDate, endDate, waterElevationDF)
+      if(input$waterType == "SW")
       {
-        waterColumnHeight <- waterColumnHeight_calc(waterDepth, input$wellDepth,input$cableLength)
-        waterElevationDF <- cbind(waterElevationDF,waterColumnHeight)
-        output$waterColumnHeightPlot <- renderPlotly({
-          waterElevationPlot <- plot_ly(waterElevationDF, x = ~Date, y = ~waterColumnHeight, type = 'scatter', mode = 'lines') %>% layout(xaxis= list(title = "Date",autotick = T,nticks = 25, tickmode = "auto"), yaxis = list(title = 'water column height'))
-        }) 
-        shinyjs::show("waterColumnHeightPlot")
-        shinyjs::show("Title_WCH")
+        regressionData <- formatReg(input, output, session)
+        waterElevationDF <- applyRegtoL0(regressionData = regressionData, L0PressureData = waterElevationDF, site = site, startDate = startDate, endDate = endDate, session)
+        output$singleWaterColumnHeight <- renderText({paste0("Calculated stage height: ",round(waterElevationDF$calculatedStage[1],3),"m")})
+      } else {
+        output$singleWaterColumnHeight <- renderText({paste0("Calculated water column height: ",round(waterElevationDF$waterColumnHeight[1],3),"m")})
       }
+
       
-    } else{ #This else is for single inputs of pressure so it will calculate differently
-      shinyjs::hide("singleWaterColumnHeight")
-      
-      #grabs the relevant calibration information from the downloaded calibrations data
-      calibration_info <- as.data.frame(rbind(calibrations$value[1:3]))
-      colnames(calibration_info) <- c("calValCP0","calValCP1","calValCP2")
-      waterElevationDF <- as.data.frame(cbind(as.numeric(input$singlePressure), as.numeric(rep(calibration_info$calValCP0)), as.numeric(rep(calibration_info$calValCP1)), as.numeric(rep(calibration_info$calValCP2)), rep(999), rep(9.81)))
-      colnames(waterElevationDF) <- c("trollPressure", "calValCP0","calValCP1","calValCP2", "waterDensity", "Gravity")
-      waterPressure <- waterPressure_calc(calValCP0 = waterElevationDF$calValCP0, calValCP1 = waterElevationDF$calValCP1,calValCP2 = waterElevationDF$calValCP2, trollPressure = waterElevationDF$trollPressure)
-      waterDepth <- waterDepth_calc(waterPressure)
-      waterElevationDF <- cbind(waterElevationDF,waterPressure,waterDepth)
-      
-      
-      if(!is.na(input$wellDepth) & !is.na(input$cableLength) & input$waterType == "GW")
-      {
-        waterColumnHeight <- waterColumnHeight_calc(waterDepth, input$wellDepth, input$cableLength)
-        output$singleWaterColumnHeight <- renderText({paste0("Current water column height: ",waterColumnHeight,"m")})
-        shinyjs::show("singleWaterColumnHeight")
-        shinyjs::show("Title_WCH")
-      }
-      
-      shinyjs::show("singlePressureOutput")
+      shinyjs::show("singleWaterColumnHeight")
       shinyjs::show("Title_CWE")
-      shinyjs::hide("waterElevation")
-      shinyjs::hide("waterColumnHeightPlot")
-      waterElevationDF$waterDepth[1] <- waterElevationDF$waterDepth[1]
-      output$singlePressureOutput <- renderText({paste0("Current water depth: ",waterElevationDF$waterDepth[1],"m")})
+      
     } #End of ifelse statement
     
-    updateProgressBar(session = session, id = "spatialDataLB", value = 100, title = paste0("All information gathered for ",input$site))
-    shinyjs::hide("LB")
-    # warning("It appears there is an issue with the settings selected. Please try different settings.")
+    updateProgressBar(session = session, id = "gaugeHeightLoadBar", value = 100, title = paste0("Finished applying relevant data to L0 pressure data for: ",input$site))
+    shinyjs::hide("gaugeHeightLoadBar")
+  }, error = function(err) {
+    updateProgressBar(session = session, id = "gaugeHeightLoadBar", value = 0, title = paste0("There was an error for: ", DPID, ", please double check that the inputs are correct"))
+    return()
   })
 }
