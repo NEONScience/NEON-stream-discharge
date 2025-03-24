577d952574c04d8ae2e2b7d3398936aede785594
znickerson8 <nickerson@battelleecology.org>
2025-03-24 08:38:43 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,8 @@
 # then runs the shiny-server. shiny-server does not pass existing
 # environment variables to the running web app, so we must use .Renviron
 
-
-echo "NEON_TOKEN=$NEON_TOKEN" >> /srv/shiny-server/.Renviron
+echo "DB_TOKEN=$DB_TOKEN" >> /srv/shiny-server/.Renviron
+echo "DB_HOST=$DB_HOST" >> /srv/shiny-server/.Renviron
 echo "HOST=$HOST" >> /srv/shiny-server/.Renviron
+echo "GITHUB_PAT=$GITHUB_PAT" >> /srv/shiny-server/.Renviron
 /usr/bin/shiny-server
